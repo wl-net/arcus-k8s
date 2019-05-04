d4395b64af3b3085ed2468d1cf6b2ae6f6be3d57
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-03 22:07:35 -0700

    Allow email to be configured, setup ui-service-ingress

--- setup-local.sh
+++ setup-local.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Installing snap..."
 sudo snap install microk8s --classic
+
+if [[ ! -e kustomize ]]; then
+  . kustomize-install.sh
+fi
+
+function prompt() {
+  local  __resultvar=$1
+  echo -n "${2} "
+  local  myresult=''
+  read myresult
+  eval $__resultvar="'$myresult'"
+}
+
+ARCUS_ADMIN_EMAIL=${ARCUS_ADMIN_EMAIL:-me@example.com}
+
+if [ "$ARCUS_ADMIN_EMAIL" = "me@example.com" ]; then
+  prompt ARCUS_ADMIN_EMAIL "Please enter your admin email address (or set ARCUS_ADMIN_EMAIL): "
+fi
+
+cp -r overlays/local-production overlays/local-production-local
+sed -i "s/me@example.com/$ARCUS_ADMIN_EMAIL/" overlays/local-production-local/cert-provider.yaml
 
 /snap/bin/microk8s.enable dns
 /snap/bin/microk8s.enable storage
 /snap/bin/microk8s.kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
 /snap/bin/microk8s.kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
 /snap/bin/microk8s.kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.7/deploy/manifests/cert-manager.yaml --validate=false
-
-. kustomize-install.sh
 
 mkdir -p secret
 if [[ ! -e secret/billing.api.key ]]; then
@@ -27,7 +47,7 @@ if [[ ! -e secret/iris.aes.iv ]]; then
   openssl rand -hex 8 > secret/iris.aes.iv
 fi
 
-if [[ ! -e secret/iris.aes.iv ]]; then
+if [[ ! -e secret/iris.aes.secret ]]; then
   echo "Generating secret for iris.aes.secret"
   openssl rand -hex 32 > secret/iris.aes.secret
 fi
@@ -57,7 +77,7 @@ set +e
 /snap/bin/microk8s.kubectl create secret generic shared --from-file secret/
 set -e
 
-./kustomize build overlays/local-production/ | /snap/bin/microk8s.kubectl apply -f -
+./kustomize build overlays/local-production-local/ | /snap/bin/microk8s.kubectl apply -f -
 
 echo "Setting up schema"
 /snap/bin/microk8s.kubectl exec cassandra-0 --stdin --tty -- '/bin/sh' '-c' 'CASSANDRA_KEYSPACE=production CASSANDRA_REPLICATION=1 /usr/bin/cassandra-provision'
