4961bab1c15d45e9e4429a73a79e402f5f4cc267
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-18 12:07:03 -0700

    move old copy to new directory

--- restore-cassandra.sh
+++ restore-cassandra.sh
@@ -2,6 +2,7 @@
 
 
 /snap/bin/microk8s.kubectl cp $1 cassandra-0:/data/"cassandra-restore.tar.gz"
+/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/mv /data/cassandra /data/cassandra-old
 /snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/tar xzvf "/data/cassandra-restore.tar.gz" 
 /snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/rm "/data/cassandra-restore.tar.gz"
 /snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/sync
