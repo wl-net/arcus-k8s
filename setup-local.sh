e5e5afe4f3e84ccc7210fcc5a0b11b7395d6ead4
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-20 20:21:53 -0700

    Remove code now in common.sh

--- setup-local.sh
+++ setup-local.sh
@@ -12,34 +12,6 @@ if [[ ! -e kustomize ]]; then
   . kustomize-install.sh
 fi
 
-function retry {
-  local retries=$1
-  shift
-
-  local count=0
-  until "$@"; do
-    exit=$?
-    wait=$((2 ** $count))
-    count=$(($count + 1))
-    if [ $count -lt $retries ]; then
-      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
-      sleep $wait
-    else
-      echo "Retry $count/$retries exited $exit, no more retries left."
-      return $exit
-    fi
-  done
-  return 0
-}
-
-function prompt() {
-  local  __resultvar=$1
-  echo -n "${2} "
-  local  myresult=''
-  read myresult
-  eval $__resultvar="'$myresult'"
-}
-
 ARCUS_ADMIN_EMAIL=${ARCUS_ADMIN_EMAIL:-me@example.com}
 
 if [ "$ARCUS_ADMIN_EMAIL" = "me@example.com" ]; then
