f373164db8635dff8da9e4b3dd8fd825e18e5fc1
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-20 20:45:29 -0700

    Allow kubectl to be replaced

--- setup-local.sh
+++ setup-local.sh
@@ -34,32 +34,32 @@ retry 6 check_k8
 retry 15 /snap/bin/microk8s.enable dns
 /snap/bin/microk8s.enable storage
 /snap/bin/microk8s.enable istio
-/snap/bin/microk8s.kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
-/snap/bin/microk8s.kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
-/snap/bin/microk8s.kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.7/deploy/manifests/cert-manager.yaml --validate=false
+$KUBECTL apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+$KUBECTL apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+$KUBECTL apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.7/deploy/manifests/cert-manager.yaml --validate=false
 
-/snap/bin/microk8s.kubectl apply -f overlays/local-production-local/metallb.yml
-/snap/bin/microk8s.kubectl apply -f localk8/cloud-generic.yaml
+$KUBECTL apply -f overlays/local-production-local/metallb.yml
+$KUBECTL apply -f localk8/cloud-generic.yaml
 
 . "${SCRIPT_DIR}/script/shared-secret.sh"
 
 set +e
-/snap/bin/microk8s.kubectl create secret generic shared --from-file secret/
+$KUBECTL create secret generic shared --from-file secret/
 set -e
 
-/snap/bin/microk8s.kubectl label namespace default istio-injection=enabled --overwrite
+$KUBECTL label namespace default istio-injection=enabled --overwrite
 function apply {
-  ./kustomize build overlays/local-production-local/ | /snap/bin/microk8s.kubectl apply -f -
+  ./kustomize build overlays/local-production-local/ | $KUBECTL apply -f -
 }
 retry 10 apply
 
 echo "Setting up schema"
-retry 10 /snap/bin/microk8s.kubectl exec cassandra-0 --stdin --tty -- '/bin/sh' '-c' 'CASSANDRA_KEYSPACE=production CASSANDRA_REPLICATION=1 CASSANDRA_HOSTNAME=localhost /usr/bin/cassandra-provision'
-kafka_id=$(/snap/bin/microk8s.kubectl get pod | grep kafka- | awk '{print $1}')
-retry 10 /snap/bin/microk8s.kubectl exec $kafka_id --stdin --tty -- '/bin/sh' '-c' 'KAFKA_REPLICATION=1 KAFKAOPS_REPLICATION=1 kafka-cmd setup'
+retry 10 $KUBECTL exec cassandra-0 --stdin --tty -- '/bin/sh' '-c' 'CASSANDRA_KEYSPACE=production CASSANDRA_REPLICATION=1 CASSANDRA_HOSTNAME=localhost /usr/bin/cassandra-provision'
+kafka_id=$($KUBECTL get pod | grep kafka- | awk '{print $1}')
+retry 10 $KUBECTL exec $kafka_id --stdin --tty -- '/bin/sh' '-c' 'KAFKA_REPLICATION=1 KAFKAOPS_REPLICATION=1 kafka-cmd setup'
 
-IPADDRESS=$(/snap/bin/microk8s.kubectl describe service -n ingress-nginx | grep 'LoadBalancer Ingress:' | awk '{print $3}')
-HUB_IPADDRESS=$(/snap/bin/microk8s.kubectl describe service hub-bridge-service | grep 'LoadBalancer Ingress:' | awk '{print $3}')
+IPADDRESS=$($KUBECTL describe service -n ingress-nginx | grep 'LoadBalancer Ingress:' | awk '{print $3}')
+HUB_IPADDRESS=$($KUBECTL describe service hub-bridge-service | grep 'LoadBalancer Ingress:' | awk '{print $3}')
 
 echo "Done with setup. Please wait a few more minutes for Arcus to start. In the mean time, please make sure you configure your DNS accordingly:"
 echo "If these IP addresses are private, you are responsible for setting up port forwarding"
