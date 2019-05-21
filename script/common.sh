bd8ce49108f5afda734f110805d6254b8a0c5c8a
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-20 20:16:02 -0700

    Move shared functions to common

---
+++ script/common.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function retry {
+  local retries=$1
+  shift
+
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** $count))
+    count=$(($count + 1))
+    if [ $count -lt $retries ]; then
+      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep $wait
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      return $exit
+    fi
+  done
+  return 0
+}
+
+function prompt() {
+  local  __resultvar=$1
+  echo -n "${2} "
+  local  myresult=''
+  read myresult
+  eval $__resultvar="'$myresult'"
+}
+
