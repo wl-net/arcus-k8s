a189f7b6922e0292f44a23556cee57eb60cfc570
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-20 20:23:58 -0700

    move shared config options

---
+++ script/shared-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ARCUS_ADMIN_EMAIL=${ARCUS_ADMIN_EMAIL:-me@example.com}
+
+if [ "$ARCUS_ADMIN_EMAIL" = "me@example.com" ]; then
+  prompt ARCUS_ADMIN_EMAIL "Please enter your admin email address (or set ARCUS_ADMIN_EMAIL): "
+fi
+
+cp -r overlays/local-production/ overlays/local-production-local
+sed -i "s/me@example.com/$ARCUS_ADMIN_EMAIL/" overlays/local-production-local/cert-provider.yaml
+
+ARCUS_DOMAIN_NAME=${ARCUS_DOMAIN_NAME:-example.com}
+
+if [ "$ARCUS_DOMAIN_NAME" = "example.com" ]; then
+  prompt ARCUS_DOMAIN_NAME "Please enter your domain name (or set ARCUS_DOMAIN_NAME): "
+fi
+cp config/shared-config/config.yml overlays/local-production-local/shared-config.yaml
+sed -i "s/arcussmarthome.com/$ARCUS_DOMAIN_NAME/" overlays/local-production-local/shared-config.yaml
--- setup-local.sh
+++ setup-local.sh
@@ -12,23 +12,7 @@ if [[ ! -e kustomize ]]; then
   . kustomize-install.sh
 fi
 
-ARCUS_ADMIN_EMAIL=${ARCUS_ADMIN_EMAIL:-me@example.com}
-
-if [ "$ARCUS_ADMIN_EMAIL" = "me@example.com" ]; then
-  prompt ARCUS_ADMIN_EMAIL "Please enter your admin email address (or set ARCUS_ADMIN_EMAIL): "
-fi
-
-cp -r overlays/local-production/ overlays/local-production-local
-sed -i "s/me@example.com/$ARCUS_ADMIN_EMAIL/" overlays/local-production-local/cert-provider.yaml
-
-ARCUS_DOMAIN_NAME=${ARCUS_DOMAIN_NAME:-example.com}
-
-if [ "$ARCUS_DOMAIN_NAME" = "example.com" ]; then
-  prompt ARCUS_DOMAIN_NAME "Please enter your domain name (or set ARCUS_DOMAIN_NAME): "
-fi
-cp config/shared-config/config.yml overlays/local-production-local/shared-config.yaml
-sed -i "s/arcussmarthome.com/$ARCUS_DOMAIN_NAME/" overlays/local-production-local/shared-config.yaml
-
+. "${SCRIPT_DIR}/script/shared-config.sh"
 
 ARCUS_SUBNET=${ARCUS_SUBNET:-unconfigured}
 
