7fda473614d2550bdaa877f410479dba5757dfad
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-26 22:55:57 -0700

    Add script to switch to a production certificate

---
+++ useprodcert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i 's/letsencrypt-staging/letsencrypt-production/g' overlays/local-production-local/ui-service-ingress.yml
+sed -i 's/nginx-staging-tls/nginx-production-tls/g' overlays/local-production-local/ui-service-ingress.yml
+$KUBECTL apply -f overlays/local-production-local/ui-service-ingress.yml
