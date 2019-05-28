b6a9eab4347ec84676a86ade49d5c68df8d73276
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-27 17:04:55 -0700

    Switch to KUBECTL environment variable

--- backup-cassandra.sh
+++ backup-cassandra.sh
@@ -2,7 +2,7 @@
 
 DATE=$(date '+%Y-%m-%d_%H-%M-%S')
 
-/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/tar zcvf "/data/cassandra-${DATE}.tar.gz" cassandra
-/snap/bin/microk8s.kubectl cp cassandra-0:/data/"cassandra-${DATE}.tar.gz" .
-/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/rm "/data/cassandra-${DATE}.tar.gz"
+$KUBECTL exec --stdin --tty cassandra-0 /bin/tar zcvf "/data/cassandra-${DATE}.tar.gz" cassandra
+$KUBECTL cp cassandra-0:/data/"cassandra-${DATE}.tar.gz" .
+$KUBECTL exec --stdin --tty cassandra-0 /bin/rm "/data/cassandra-${DATE}.tar.gz"
 
