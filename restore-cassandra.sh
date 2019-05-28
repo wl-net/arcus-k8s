f85a43491d542f926a0a39c7ecf81704a3831606
Andrew Sorensen <andrew@andrewsorensen.net>
2019-05-27 18:48:07 -0700

    Switch to using KUBECTL environment variable

--- restore-cassandra.sh
+++ restore-cassandra.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
-/snap/bin/microk8s.kubectl cp $1 cassandra-0:/data/"cassandra-restore.tar.gz"
-/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/mv /data/cassandra /data/cassandra-old
-/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/tar xzvf "/data/cassandra-restore.tar.gz" 
-/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/rm "/data/cassandra-restore.tar.gz"
-/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /bin/sync
-/snap/bin/microk8s.kubectl exec --stdin --tty cassandra-0 /usr/bin/killall java
+
+$KUBECTL cp $1 cassandra-0:/data/"cassandra-restore.tar.gz"
+$KUBECTL exec --stdin --tty cassandra-0 /bin/mv /data/cassandra /data/cassandra-old
+$KUBECTL exec --stdin --tty cassandra-0 /bin/tar xzvf "/data/cassandra-restore.tar.gz"
+$KUBECTL exec --stdin --tty cassandra-0 /bin/rm "/data/cassandra-restore.tar.gz"
+$KUBECTL exec --stdin --tty cassandra-0 /bin/sync
+$KUBECTL exec --stdin --tty cassandra-0 /usr/bin/killall java
