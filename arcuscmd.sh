4fec4013b8793c6ffedc55af77cfc8550653468c
Andrew Sorensen <andrew@andrewsorensen.net>
2019-10-03 18:20:35 -0700

    Add additional tools

--- arcuscmd.sh
+++ arcuscmd.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 set -euo pipefail
 
-. script/funcs.sh
+SCRIPT_PATH="$0"
+SCRIPT_DIR=$(dirname ${SCRIPT_PATH})
+. "${SCRIPT_DIR}/script/common.sh"
+. "${SCRIPT_DIR}/script/funcs.sh"
+
+# setup
+
+set +e
+ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "Couldn't get root of git repository. You must checkout arcus-k8 as a git repository, not as an extracted zip."
+  exit $RESULT
+fi
+set -e
+
+ARCUS_CONFIGDIR="${ROOT}/.config"
+mkdir -p $ARCUS_CONFIGDIR
 
 KUBECTL=${KUBECTL:-kubectl}
+
+if [ -x "$(command -v microk8s.kubectl)" ]; then
+  KUBECTL=microk8s.kubectl
+fi
+
+function print_available {
+  cat << ENDOFDOC
+arcuscmd: manage your arcus deployment
+
+Basic Commands:
+  install    - install microk8s for local (on-prem) deployment
+  configure  - configure arcus by answering a few questions
+  apply      - apply the existing configured configuration
+  deploy     - deploy arcus (rolling the entire fleet, 1 service at a time)
+  update     - update your local copy with the latest changes
+ENDOFDOC
+
+}
 
 cmd=${1:-help}
 
@@ -12,7 +47,13 @@ case "$cmd" in
             echo "setup local"
 	;;
         apply)
-             ./kustomize build overlays/local-production-local/ | $KUBECTL apply -f -
+             apply
+        ;;
+	install)
+             install
+        ;;
+        configure)
+	     configure
         ;;
         deploy)
              deployfast
@@ -26,7 +67,16 @@ case "$cmd" in
 	useprodcert)
 	     useprodcert
 	;;
+        info)
+	     info
+        ;;
+        update)
+             update
+        ;;
+        help)
+             print_available
+        ;;
         *)
-            echo "unsupported command"
+            echo "unsupported command: $cmd"
+	    print_available
 esac
-
--- script/funcs.sh
+++ script/funcs.sh
@@ -1,3 +1,5 @@
+# shared functions
+
 function updatehubkeystore {
         echo "Creating hub-keystore..."
 	mkdir -p converted
@@ -47,4 +49,84 @@ function deployfast {
             $KUBECTL delete pod -l app=$app
 	    sleep 5
 	done
+}
+
+function install {
+   # TODO: separate local/cloud
+   retry 6 check_k8
+
+   retry 15 /snap/bin/microk8s.enable dns
+   /snap/bin/microk8s.enable storage
+   /snap/bin/microk8s.enable istio
+
+   $KUBECTL create namespace cert-manager
+   $KUBECTL label namespace cert-manager certmanager.k8s.io/disable-validation=true
+
+   $KUBECTL apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
+   $KUBECTL apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.0/deploy/static/mandatory.yaml
+   $KUBECTL apply -f https://github.com/jetstack/cert-manager/releases/download/v0.10.1/cert-manager.yaml
+
+   $KUBECTL apply -f overlays/local-production-local/metallb.yml
+}
+
+function info {
+  load
+  IPADDRESS=$($KUBECTL describe service -n ingress-nginx | grep 'LoadBalancer Ingress:' | awk '{print $3}')
+  HUB_IPADDRESS=$($KUBECTL describe service hub-bridge-service | grep 'LoadBalancer Ingress:' | awk '{print $3}')
+
+  echo "DNS -> IP/Port Mappings: "
+  echo "If these IP addresses are private, you are responsible for setting up port forwarding"
+  echo "${ARCUS_DOMAIN_NAME}:80 -> $IPADDRESS:80"
+  echo "${ARCUS_DOMAIN_NAME}:443 -> $IPADDRESS:443"
+  echo "client.${ARCUS_DOMAIN_NAME}:443 -> $IPADDRESS:443"
+  echo "static.${ARCUS_DOMAIN_NAME}:443 -> $IPADDRESS:443"
+  echo "ipcd.${ARCUS_DOMAIN_NAME}:443 -> $IPADDRESS:443"
+  echo "admin.${ARCUS_DOMAIN_NAME}:443 -> $IPADDRESS:443"
+  echo "hub.${ARCUS_DOMAIN_NAME}:443 -> $IPADDRESS:443"
+}
+
+function load {
+  ARCUS_ADMIN_EMAIL=$(cat $ARCUS_CONFIGDIR/admin.email)
+  ARCUS_DOMAIN_NAME=$(cat $ARCUS_CONFIGDIR/domain.name)
+}
+
+function apply {
+  # Apply the configuration
+  load
+
+  mkdir -p overlays/local-production-local
+  cp -r overlays/local-production/* overlays/local-production-local
+
+  sed -i "s/me@example.com/$ARCUS_ADMIN_EMAIL/" overlays/local-production-local/cert-provider.yaml
+
+  cp config/shared-config/config.yml overlays/local-production-local/shared-config.yaml
+  sed -i "s/arcussmarthome.com/$ARCUS_DOMAIN_NAME/" overlays/local-production-local/shared-config.yaml
+
+  cp config/service/ui-service-ingress.yml overlays/local-production-local/ui-service-ingress.yml
+  sed -i "s/arcussmarthome.com/$ARCUS_DOMAIN_NAME/" overlays/local-production-local/ui-service-ingress.yml
+
+ ./kustomize build overlays/local-production-local/ | $KUBECTL apply -f -
+}
+
+function configure {
+  ARCUS_ADMIN_EMAIL=${ARCUS_ADMIN_EMAIL:-me@example.com}
+  ARCUS_DOMAIN_NAME=${ARCUS_DOMAIN_NAME:-example.com}
+
+  if [ "$ARCUS_ADMIN_EMAIL" = "me@example.com" ]; then
+    prompt ARCUS_ADMIN_EMAIL "Please enter your admin email address (or set ARCUS_ADMIN_EMAIL): "
+  fi
+  echo $ARCUS_ADMIN_EMAIL > $ARCUS_CONFIGDIR/admin.email
+
+  if [ "$ARCUS_DOMAIN_NAME" = "example.com" ]; then
+    prompt ARCUS_DOMAIN_NAME "Please enter your domain name (or set ARCUS_DOMAIN_NAME): "
+  fi
+  echo $ARCUS_DOMAIN_NAME > $ARCUS_CONFIGDIR/domain.name
+}
+
+function update {
+  cd $ROOT
+  git fetch
+  git pull
+  cd - >/dev/null
+  echo "on $(git rev-parse --abbrev-ref HEAD)"
 }
