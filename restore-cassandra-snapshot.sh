ac939673a5204a2c4bb35888da7947d2a6b2b1f5
Andrew Sorensen <andrew@andrewsorensen.net>
2019-10-30 19:57:10 -0700

    Create directory

--- restore-cassandra-snapshot.sh
+++ restore-cassandra-snapshot.sh
@@ -4,6 +4,7 @@ KUBECTL=${KUBECTL:-kubectl}
 
 $KUBECTL cp $1 cassandra-0:/data/"cassandra-restore.tar.gz"
 #$KUBECTL exec --stdin --tty cassandra-0 /bin/mv /data/cassandra /data/cassandra-old
+$KUBECTL exec --stdin --tty cassandra-0 -- /bin/mkdir /data/restore
 $KUBECTL exec --stdin --tty cassandra-0 -- /bin/tar xzvf "/data/cassandra-restore.tar.gz" -C /data/restore
 $KUBECTL exec --stdin --tty cassandra-0 -- /bin/rm "/data/cassandra-restore.tar.gz"
 $KUBECTL exec --stdin --tty cassandra-0 -- /bin/bash -c "cd /data/restore; /usr/bin/cqlsh < /data/restore/keyspaces.cqlsh" 
