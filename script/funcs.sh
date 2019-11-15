d439740972f925592d8bdef63846f29448074991
Andrew Sorensen <andrew@andrewsorensen.net>
2019-11-14 22:51:27 -0800

    Remove 'build'

--- script/funcs.sh
+++ script/funcs.sh
@@ -215,7 +215,7 @@ function apply() {
 
   $KUBECTL apply -f config/certprovider/
 
-  $KUBECTL kustomize build overlays/local-production-local/ | $KUBECTL apply -f -
+  $KUBECTL kustomize overlays/local-production-local/ | $KUBECTL apply -f -
 }
 
 function configure() {
