ab081c99d2887a39ca4fa93a4d58bc31f381377f
Andrew Sorensen <andrew@andrewsorensen.net>
2020-12-11 05:20:53 +0000

    Fix certificate issue

--- script/funcs.sh
+++ script/funcs.sh
@@ -3,8 +3,8 @@
 function updatehubkeystore() {
   echo "Creating hub-keystore..."
   mkdir -p converted
-  KUBE_EDITOR=cat $KUBECTL edit secret nginx-production-tls 2>/dev/null | grep tls.key | awk '{print $2}' | base64 -d >converted/orig.key
-  KUBE_EDITOR=cat $KUBECTL edit secret nginx-production-tls 2>/dev/null | grep tls.crt | awk '{print $2}' | base64 -d >converted/tls.crt
+  KUBE_EDITOR=cat $KUBECTL edit secret nginx-production-tls 2>/dev/null | grep tls.key | awk '{print $2}' | head -n 1 | base64 -d >converted/orig.key
+  KUBE_EDITOR=cat $KUBECTL edit secret nginx-production-tls 2>/dev/null | grep tls.crt | awk '{print $2}' | head -n 1 | base64 -d >converted/tls.crt
 
   openssl pkcs8 -in converted/orig.key -topk8 -nocrypt -out converted/tls.key
   rm converted/orig.key
